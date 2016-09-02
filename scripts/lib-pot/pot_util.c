d0f1b2f5b39bb2c4645288576eb8a7bcabf491d7
Shwetha Bhandari <shwethab@cisco.com>
2016-09-02 11:15:07 +0100

    Library to compute cumulative and verify proof of transit and test code to parse the configs and create the profiles and verify the library
    Signed-off-by: Shwetha Bhandari <shwethab@cisco.com>

---
+++ scripts/lib-pot/pot_util.c
@@ -0,0 +1,310 @@
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include "pot_util.h"
+#include "math64.h"
+
+pot_main_t pot_main;
+
+static void pot_profile_cleanup (pot_profile * profile);
+
+static void
+pot_main_profiles_reset (void)
+{
+  pot_main_t *sm = &pot_main;
+  int i = 0;
+
+  for (i = 0; i < MAX_POT_PROFILES; i++)
+    {
+      pot_profile_cleanup (&(sm->profile_list[i]));
+    }
+  sm->active_profile_id = 0;
+  sm->profile_list_name[0] = 0;
+}
+
+int
+pot_util_init (void)
+{
+  pot_main_profiles_reset ();
+
+  return (0);
+}
+
+static void
+pot_profile_init (pot_profile * new, u8 id)
+{
+  if (new)
+    {
+      memset (new, 0, sizeof (pot_profile));
+      new->id = id;
+    }
+}
+
+pot_profile *
+pot_profile_find (u8 id)
+{
+  pot_main_t *sm = &pot_main;
+
+  if (id < MAX_POT_PROFILES)
+    {
+      return (&(sm->profile_list[id]));
+    }
+  return (NULL);
+}
+
+static int
+pot_profile_name_equal (u8 * name0, u8 * name1)
+{
+  int len0, len1;
+
+  len0 = strlen (name0);
+  len1 = strlen (name1);
+  if (len0 != len1)
+    return (0);
+  return (0 == strncmp ((char *) name0, (char *) name1, len0));
+}
+
+int
+pot_profile_list_is_enabled (u8 * name)
+{
+  pot_main_t *sm = &pot_main;
+  return (pot_profile_name_equal (sm->profile_list_name, name));
+}
+
+void
+pot_profile_list_init (u8 * profile_list_name)
+{
+  pot_main_t *sm = &pot_main;
+  int i = 0;
+
+  /* If it is the same profile list skip reset */
+  if (pot_profile_name_equal (sm->profile_list_name, profile_list_name))
+    {
+      return;
+    }
+
+  pot_main_profiles_reset ();
+  strncpy (&sm->profile_list_name[0], profile_list_name, NAME_LEN);
+  sm->active_profile_id = 0;
+
+  for (i = 0; i < MAX_POT_PROFILES; i++)
+    {
+      pot_profile_init (&(sm->profile_list[i]), i);
+    }
+}
+
+static void
+pot_profile_cleanup (pot_profile * profile)
+{
+  u16 id = profile->id;
+
+  memset (profile, 0, sizeof (pot_profile));
+  profile->id = id;		/* Restore id alone */
+}
+
+int
+pot_profile_create (pot_profile * profile, u64 prime,
+		    u64 poly2, u64 lpc, u64 secret_share)
+{
+  if (profile && !profile->in_use)
+    {
+      pot_profile_cleanup (profile);
+      profile->prime = prime;
+      profile->primeinv = 1.0 / prime;
+      profile->lpc = lpc;
+      profile->poly_pre_eval = poly2;
+      profile->secret_share = secret_share;
+      profile->total_pkts_using_this_profile = 0;
+      profile->valid = 1;
+      return (0);
+    }
+
+  return (-1);
+}
+
+int
+pot_set_validator (pot_profile * profile, u64 key)
+{
+  if (profile && !profile->in_use)
+    {
+      profile->validator = 1;
+      profile->secret_key = key;
+      return (0);
+    }
+  return (-1);
+}
+
+u64
+pot_update_cumulative_inline (u64 cumulative, u64 random,
+			      u64 secret_share, u64 prime, u64 lpc,
+			      u64 pre_split, double prime_inv)
+{
+  u64 share_random = 0;
+  u64 cumulative_new = 0;
+
+  /*
+   * calculate split share for random
+   */
+  share_random = add64_mod (pre_split, random, prime, prime_inv);
+
+  /*
+   * lpc * (share_secret + share_random)
+   */
+  share_random = add64_mod (share_random, secret_share, prime, prime_inv);
+  share_random = mul64_mod (share_random, lpc, prime, prime_inv);
+
+  cumulative_new = add64_mod (cumulative, share_random, prime, prime_inv);
+
+  return (cumulative_new);
+}
+
+u64
+pot_update_cumulative (pot_profile * profile, u64 cumulative, u64 random)
+{
+  if (profile && profile->valid != 0)
+    {
+      return (pot_update_cumulative_inline
+	      (cumulative, random, profile->secret_share, profile->prime,
+	       profile->lpc, profile->poly_pre_eval, profile->primeinv));
+    }
+  return (0);
+}
+
+u8
+pot_validate_inline (u64 secret, u64 prime, double prime_inv,
+		     u64 cumulative, u64 random)
+{
+  if (cumulative == (random + secret))
+    {
+      return (1);
+    }
+  else if (cumulative == add64_mod (random, secret, prime, prime_inv))
+    {
+      return (1);
+    }
+  return (0);
+}
+
+/*
+ * return True if the cumulative matches secret from a profile
+ */
+u8
+pot_validate (pot_profile * profile, u64 cumulative, u64 random)
+{
+  if (profile && profile->validator)
+    {
+      return (pot_validate_inline (profile->secret_key, profile->prime,
+				   profile->primeinv, cumulative, random));
+    }
+  return (0);
+}
+
+/*
+ * Utility function to get random number per pack
+ */
+u64
+pot_generate_random (pot_profile * profile)
+{
+  u64 random = 0;
+  int32_t second_half;
+
+  /*
+   * Upper 4 bytes seconds
+   */
+  random = (u64) time (NULL);
+
+  random &= 0xffffffff;
+  random = random << 32;
+  /*
+   * Lower 4 bytes random number
+   */
+  srand (time (NULL));
+  second_half = rand ();
+  random |= second_half;
+
+  if (profile != NULL)
+    {
+      random &= profile->bit_mask;
+    }
+  return (random);
+}
+
+int
+pot_profile_set_bit_mask (pot_profile * profile, u16 bits)
+{
+  int sizeInBits;
+
+  if (profile && !profile->in_use)
+    {
+      sizeInBits = sizeof (profile->bit_mask) * 8;
+      profile->bit_mask =
+	(bits >= sizeInBits ? (u64) - 1 : (u64) ((u64) 1 << (u64) bits) - 1);
+      return (0);
+    }
+  return (-1);
+}
+
+/* Function to get no of set bits in binary
+   representation of passed binary no. */
+static int
+countSetBits (uint64_t n)
+{
+  unsigned int count = 0;
+  while (n)
+    {
+      n &= (n - 1);
+      count++;
+    }
+  return count;
+}
+
+int
+pot_profile_to_str (pot_profile * profile, char *buf, int n)
+{
+  int written = 0;
+  int total_written = 0;
+  int i = 0;
+  int no_of_ser = 0;
+  if (profile && buf)
+    {
+      total_written += written =
+	snprintf (buf, n, "<sc-profile> profile-id %d ", profile->id);
+      n -= written;
+      buf += written;
+      if (profile->validator)
+	{
+	  total_written += written = snprintf (buf, n, "validate-key 0x%llx ",
+					       profile->secret_key);
+	  n -= written;
+	  buf += written;
+	}
+      total_written += written =
+	snprintf (buf, n,
+		  "prime-number 0x%llx secret_share 0x%llx lpc 0x%llx polynomial2 0x%llx ",
+		  profile->prime, profile->secret_share, profile->lpc,
+		  profile->poly_pre_eval);
+      n -= written;
+      buf += written;
+      total_written += written =
+	snprintf (buf, n, "bits-in-random %d</sc-profile>\n",
+		  countSetBits (profile->bit_mask));
+      n -= written;
+      buf += written;
+    }
+  return total_written;
+}
---
+++ scripts/lib-pot/test.c
@@ -0,0 +1,230 @@
+/*
+ * Copyright (c) 2016 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include "pot_util.h"
+
+#define MAX_STR_LEN 300
+#define MAX_SERVICES 10
+
+pot_profile profile[MAX_SERVICES];
+
+/* return the current arg, and advance the buff to the next space-separated word */
+char *
+get_next_arg (char **pbuff)
+{
+  char *pc = NULL;
+  while ((*pbuff) && (**pbuff) && ((**pbuff == ' ') || (**pbuff == '\n')))
+    {
+      **pbuff = 0;
+      (*pbuff)++;
+    }
+  pc = *pbuff;
+
+  while ((*pbuff) && (**pbuff) && ((**pbuff != ' ') && (**pbuff != '\n')))
+    {
+      (*pbuff)++;
+    }
+
+  while ((*pbuff) && (**pbuff) && ((**pbuff == ' ') || (**pbuff == '\n')))
+    {
+      **pbuff = 0;
+      (*pbuff)++;
+    }
+
+  if ((pc) && (0 == *pc))
+    {
+      pc = NULL;
+    }
+  return pc;
+}
+
+static int
+strtou16 (char *num, int base, u16 * result)
+{
+  *result = (u16) strtol (num, NULL, base);
+  return (0);
+}
+
+static int
+strtou64 (char *num, int base, u64 * result)
+{
+  *result = (u64) strtoll (num, NULL, base);
+  return (0);
+}
+
+static void
+pot_config_parse (char *buf, int profile_no)
+{
+  u64 prime;
+  u64 secret_share;
+  u64 secret_key;
+  u16 my_service_index = 0;
+  char *buff;
+  char *arg_name;
+  u16 mark = 0;
+  char *num = NULL;
+  bool validator = false;
+  int i;
+  u16 id;
+  u16 bits;
+  u64 lpc;
+  u64 poly2;
+
+  buff = buf;
+  arg_name = NULL;
+
+
+  while (NULL != (arg_name = get_next_arg (&buff)))
+    {
+      num = NULL;
+      if (0 == strcmp (arg_name, "id"))
+	{
+	  num = get_next_arg (&buff);
+	  id = 0;
+	  if (num && (0 == strtou16 (num, 0, &id)))
+	    {
+	    }
+	}
+      else if (0 == strcmp (arg_name, "validate-key"))
+	{
+	  num = get_next_arg (&buff);
+	  if (num && (0 == strtou64 (num, 0, (u64 *) & secret_key)))
+	    {
+	      validator = true;
+	    }
+	}
+      else if (0 == strcmp (arg_name, "lpc"))
+	{
+	  num = get_next_arg (&buff);
+	  if (num && (0 == strtou64 (num, 0, (u64 *) & lpc)))
+	    {
+	    }
+	}
+      else if (0 == strcmp (arg_name, "polynomial2"))
+	{
+	  num = get_next_arg (&buff);
+	  if (num && (0 == strtou64 (num, 0, (u64 *) & poly2)))
+	    {
+	    }
+	}
+      else if (0 == strcmp (arg_name, "prime-number"))
+	{
+	  num = get_next_arg (&buff);
+	  if (num && (0 == strtou64 (num, 0, (u64 *) & prime)))
+	    {
+	    }
+	}
+      else if (0 == strcmp (arg_name, "secret_share"))
+	{
+	  num = get_next_arg (&buff);
+	  if (num && (0 == strtou64 (num, 0, (u64 *) & secret_share)))
+	    {
+	    }
+	}
+      else if (0 == strcmp (arg_name, "bits-in-random"))
+	{
+	  num = get_next_arg (&buff);
+	  bits = MAX_BITS;
+	  if (num && (0 == strtou16 (num, 0, &bits)))
+	    {
+	      if (bits > MAX_BITS)
+		{
+		  bits = MAX_BITS;
+		}
+	    }
+	}
+      else
+	{
+	  printf ("Skipping \"%s\"\n", arg_name);
+	}
+
+    }
+  pot_profile_create (&profile[profile_no], prime, poly2, lpc, secret_share);
+  if (validator)
+    pot_set_validator (&profile[profile_no], secret_key);
+  pot_profile_set_bit_mask (&profile[profile_no], bits);
+}
+
+#define BUFSIZE 4000
+
+int
+main (int argc, char *argv[])
+{
+  FILE *fp = NULL;
+  char buff[BUFSIZE];
+  u64 random = 0;
+  u64 cumulative = 0;
+  int count = 0;
+  int i;
+  char *cmd = "set pot profile";
+  char *tail = buff;
+  fp = fopen (argv[1], "r");
+  pot_util_init ();
+  while (fp && fgets (buff, BUFSIZE - 1, fp) != NULL)
+    {
+      tail = buff;
+      if (0 != (tail = strstr (tail, cmd)))
+	{
+	  /*
+	   * set pot profile name example id 0 prime-number 0xffffffbd3f0331
+	   * secret_share 0x23dc164fa011aa lpc 0x1 polynomial2 0x5dc0a0163d348
+	   * bits-in-random 56
+	   */
+	  tail += strlen (cmd);
+	  pot_config_parse (tail, count);
+	  count++;
+
+
+	}
+    }
+  fclose (fp);
+  if (count == 0)
+    {
+      printf ("\n Unable to parse profile \n");
+      exit (1);
+    }
+  for (i = 0; i < count; i++)
+    {
+      pot_profile_to_str (&profile[i], buff, BUFSIZE - 1);
+      printf ("\n%s", buff);
+
+    }
+  random = pot_generate_random (&profile[0]);
+  cumulative = 0;
+  for (i = 0; i < count; i++)
+    {
+      cumulative = pot_update_cumulative (&profile[i], cumulative, random);
+      printf
+	("\n Cumulative for random %lld (0x%llx) is %llu (0x%llx) @pass %d \n",
+	 random, random, cumulative, cumulative, i + 1);
+    }
+  printf ("\n Now Verifying...\n");
+  if (pot_validate (&profile[count - 1], cumulative, random))
+    {
+      printf ("\n Valid cumulative... verification result passed");
+    }
+  else
+    {
+      printf ("\n Invalid cumulative.. verification result failed");
+    }
+
+  printf ("\n");
+
+}
