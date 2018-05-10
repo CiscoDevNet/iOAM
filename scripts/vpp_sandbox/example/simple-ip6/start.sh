1523d73b3716f7c62fc02f6e943e662ff721c0cd
Rakesh Kandula <rrkandula@gmail.com>
2018-05-09 21:55:59 -0700

    Fixing the VPP instance crash on slow VM's
    Signed-off-by: Rakesh Kandula <rrkandula@gmail.com>

--- scripts/vpp_sandbox/example/simple-ip6/start.sh
+++ scripts/vpp_sandbox/example/simple-ip6/start.sh
@@ -6,9 +6,9 @@ sudo ./launch_lxc.sh -n b -t vpp-ext -l 2
 sudo ./launch_lxc.sh -n c -t vpp-ext -l 2
 sudo ./launch_lxc.sh -n host2 -t vpp-ext -l 2
 sudo ./connect_lxc.sh -c ./example/simple-ip6/config.txt -f ./example/simple-ip6/connect.log
-lxc-attach -n a -- vpp unix { log /tmp/vpp.log full-coredump startup-config /scratch/example/simple-ip6/a.conf cli-listen localhost:5002 }
-lxc-attach -n b -- vpp unix { log /tmp/vpp.log full-coredump startup-config /scratch/example/simple-ip6/b.conf cli-listen localhost:5002 }
-lxc-attach -n c -- vpp unix { log /tmp/vpp.log full-coredump startup-config /scratch/example/simple-ip6/c.conf cli-listen localhost:5002 }
+lxc-attach -n a -- bash -c 'vpp unix { log /tmp/vpp.log full-coredump startup-config /scratch/example/simple-ip6/a.conf cli-listen localhost:5002 }; echo "Sleeping for 10 seconds"; sleep 10'
+lxc-attach -n b -- bash -c 'vpp unix { log /tmp/vpp.log full-coredump startup-config /scratch/example/simple-ip6/b.conf cli-listen localhost:5002 }; echo "Sleeping for 10 seconds"; sleep 10'
+lxc-attach -n c -- bash -c 'vpp unix { log /tmp/vpp.log full-coredump startup-config /scratch/example/simple-ip6/c.conf cli-listen localhost:5002 }; echo "Sleeping for 10 seconds"; sleep 10'
 lxc-attach -n host1 -- ip -6 address add db00::2/64 dev l_host11
 lxc-attach -n host1 -- ip -6 route add default via db00::1
 lxc-attach -n host2 -- ip -6 address add db03::2/64 dev l_host21
